b9a5fbe1cfa3b07b790323c1480b7b788d02a8d5
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-09 10:03:17 -0400

    Time: 22 ms (99.69%), Space: 17.7 MB (27.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,27 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Time Complexity: O(n)
-        # Iterative solution 
-        # prev, curr = None, head 
+        prev, curr = None, head 
 
-        # while curr:
-        #     # Temporary variable 
-        #     nxt = curr.next 
-        #     curr.next = prev 
-        #     # update the pointers 
-        #     prev = curr 
-        #     curr = nxt 
-        # return prev 
-        # Time Complexity: O(n)
-        # Recursive solution 
-        if not head: 
-            return None 
-
-        newHead = head 
-        if head.next: 
-            newHead = self.reverseList(head.next)
-            head.next.next = head 
-        head.next = None 
-
-        return newHead 
+        while curr:
+            nxt = curr.next 
+            curr.next = prev
+            prev = curr 
+            curr = nxt 
+        return prev 
+        
